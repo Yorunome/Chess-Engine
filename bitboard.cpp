b8b66d7216a0df8177c059032f0de226712222c9
Yorunome <mizuhashi05@gmail.com>
2019-03-04 00:42:45 +0530

    added the set and clear mask functions

--- bitboard.cpp
+++ bitboard.cpp
@@ -2,6 +2,34 @@
 
 using namespace std;
 
+//array with 64 integers
+const int bitTable[64] = { 
+	63, 30,  3, 32, 25, 41, 22, 33, 
+	15, 50, 42, 13, 11, 53, 19, 34,
+	61, 29,  2, 51, 21, 43, 45, 10,
+	18, 47,  1, 54,  9, 57,  0, 35, 
+	62, 31, 40,  4, 49,  5, 52,	26,
+	60,  6, 23, 44, 46, 27, 56, 16,
+	 7, 39, 48, 24, 59, 14, 12, 55,
+	38, 28, 58, 20, 37, 17, 36,  8
+};
+
+//takes a pointer to bitboard, starting from LSB
+//finds the index where the first bit is set and then removes it
+int popBit(U64 *bb){
+	U64 b = *bb ^ (*bb - 1);
+	unsigned int fold = (unsigned)((b & 0xffffffff));
+	*bb &= (*bb - 1);
+	return bitTable[(fold * 0x783a9b23) >> 26];
+}
+
+//takes a bitboard and count the number of set bits
+int countBits(U64 b){
+	int r;
+	for (r = 0; b; r++, b &= b - 1);
+	return r;
+}
+
 void printBitBoard(U64 bb){
 	U64 shiftMe = 1ULL;			//for the shift operations
 
